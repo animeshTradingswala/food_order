c6dbba3aa164e18d1a8981f8aa4270c68d1d7bac
USER <animesh.banerjee@tradingswala.com>
2021-11-08 23:06:39 -0800

    done

---
+++ src/navigation/navigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screen/home';
+import Detail from '../screen/details';
+
+
+const Stack = createStackNavigator();
+
+const screenOptionStyle = {
+    headerShown : false
+}
+
+const HomeStackNavigator = () => {
+    return(
+        <Stack.Navigator screenOptions={screenOptionStyle}>
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Detail" component={Detail}/>
+        </Stack.Navigator>
+    )
+}
+
+export default HomeStackNavigator;
